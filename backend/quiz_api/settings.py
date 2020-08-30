fd7ac55eb2c842d0e81947743498833c39ec18c1
Izen Huang <izenhuang@hotmail.com>
2020-08-30 08:58:46 -0700

    feat: default authentication class

--- backend/quiz_api/settings.py
+++ backend/quiz_api/settings.py
@@ -121,8 +121,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
+# REST Framework
+
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
-    ]
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly'
+    )
 }
