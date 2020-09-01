aa202373a6763d379bfc54f8ce661c7e134dc984
Izen Huang <izenhuang@hotmail.com>
2020-09-01 16:30:26 -0700

    feat: redux action update chosen quiz id

--- frontend/src/redux/ActionCreators.js
+++ frontend/src/redux/ActionCreators.js
@@ -1,15 +1,22 @@
 import * as ActionTypes from './ActionTypes';
 
+export const updateQuizId = (newQuizId) => ({
+	type: ActionTypes.UPDATE_CHOSEN_QUIZ_ID,
+	payload: {
+		newQuizId: newQuizId
+	}
+})
+
 export const updateReviewList = (newReviewList) => ({
-  type: ActionTypes.UPDATE_REVIEW_LIST,
-  payload: {
-    newReviewList: newReviewList
-  }
+	type: ActionTypes.UPDATE_REVIEW_LIST,
+	payload: {
+		newReviewList: newReviewList
+	}
 });
 
 export const updateUserAnswers = (newUserAnswers) => ({
-  type: ActionTypes.UPDATE_USER_ANSWERS,
-  payload: {
-    newUserAnswers: newUserAnswers
-  }
+	type: ActionTypes.UPDATE_USER_ANSWERS,
+	payload: {
+		newUserAnswers: newUserAnswers
+	}
 });
--- frontend/src/redux/reducer.js
+++ frontend/src/redux/reducer.js
@@ -1,27 +1,37 @@
 import * as ActionTypes from './ActionTypes';
 
 export const initialState = {
-  reviewList: [],
-  userAnswers: {},
-  totalQuestions: 2
+	reviewList: [],
+	userAnswers: {},
+	chosenQuizId: undefined,
+	totalQuestions: 2
 }
 
 export const Reducer = (state = initialState, action) => {
-  const { UPDATE_REVIEW_LIST, UPDATE_USER_ANSWERS } = ActionTypes;
-  const { payload } = action;
+	const { 
+		UPDATE_CHOSEN_QUIZ_ID, 
+		UPDATE_REVIEW_LIST, 
+		UPDATE_USER_ANSWERS 
+	} = ActionTypes;
+	const { payload } = action;
 
-  switch (action.type) {
-    case (UPDATE_REVIEW_LIST):
-      return {
-        ...state,
-        reviewList: payload.newReviewList
-      }
-    case (UPDATE_USER_ANSWERS):
-      return {
-        ...state,
-        userAnswers: payload.newUserAnswers
-      }
-    default: 
-      return state;
-  }
+	switch (action.type) {
+		case (UPDATE_CHOSEN_QUIZ_ID):
+			return {
+				...state,
+				chosenQuizId: payload.newQuizId
+			}
+		case (UPDATE_REVIEW_LIST):
+			return {
+				...state,
+				reviewList: payload.newReviewList
+			}
+		case (UPDATE_USER_ANSWERS):
+			return {
+				...state,
+				userAnswers: payload.newUserAnswers
+			}
+		default: 
+			return state;
+	}
 }
