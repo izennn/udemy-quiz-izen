db5141143adeae559bb1fba431ba895a3cd31204
izen.oku <izenhuang@hotmail.com>
2020-09-08 19:23:17 -0700

    feat: whitenoise to middleware and requirements.txt

--- quiz_api/settings.py
+++ quiz_api/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
